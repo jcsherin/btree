efffa743263497e1289c66cc02139f669b98fee0
Jacob Sherin <jacob@protoship.io>
2023-10-13 20:40:41 +0530

    defines node max and min sizes

--- main.cpp
+++ main.cpp
@@ -1,11 +1,60 @@
 #include <iostream>
+#include <cmath>
 
 enum class NodeType : int {
-    InternalType = 0, LeafType = 1
+    InternalNode = 0, LeafNode = 1
+};
+
+class BaseNode {
+public:
+    /**
+     * Constructor
+     */
+    BaseNode() = default;
+
+    /**
+     * Destructor
+     */
+    ~BaseNode() = default;
+
+    /**
+     *
+     * @return the size for internal node split [fanout]
+     */
+    int GetInternalNodeMaxSize() const { return internal_node_max_size_; }
+
+    /**
+     *
+     * @return the size for internal node removal [⌈fanout / 2⌉ - 1]
+     */
+    int GetInternalNodeMinSize() const { return internal_node_min_size_; }
+
+    /**
+     *
+     * @return the size for leaf node split [fanout]
+     */
+    int GetLeafNodeMaxSize() const { return leaf_node_max_size_; }
+
+    /**
+     *
+     * @return the size for leaf node removal [⌈(fanout - 1) / 2⌉]
+     */
+    int GetLeafNodeMinSize() const { return leaf_node_min_size_; }
+
+private:
+    int internal_node_max_size_ = 128;
+    int internal_node_min_size_ = std::ceil(internal_node_max_size_ / 2) - 1;
+
+    int leaf_node_max_size_ = 128;
+    int leaf_node_min_size_ = std::ceil((leaf_node_max_size_ - 1) / 2);
 };
 
 int main() {
-    std::cout << "Internal Node: " << static_cast<int>(NodeType::InternalType) << std::endl;
-    std::cout << "Leaf Node: " << static_cast<int>(NodeType::LeafType) << std::endl;
+    auto b = BaseNode();
+
+    std::cout << "Internal node max: " << b.GetInternalNodeMaxSize() << " min: " << b.GetInternalNodeMinSize()
+              << std::endl;
+    std::cout << "Leaf node max: " << b.GetLeafNodeMaxSize() << " min: " << b.GetLeafNodeMinSize() << std::endl;
+
     return 0;
 }

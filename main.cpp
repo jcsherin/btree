89a04ed85c6fbc2f7d698bf6cb4c588a03a17eca
Jacob Sherin <jacob@protoship.io>
2023-10-13 19:44:28 +0530

    adds internal & leaf node types

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+enum class NodeType : int {
+    InternalType = 0, LeafType = 1
+};
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Internal Node: " << static_cast<int>(NodeType::InternalType) << std::endl;
+    std::cout << "Leaf Node: " << static_cast<int>(NodeType::LeafType) << std::endl;
     return 0;
 }

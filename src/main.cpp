5213124fd13573cb8d75b35a0a36f195825e131f
Jacob Sherin <jacob@protoship.io>
2023-10-17 01:00:12 +0530

    removes main

--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "btree.h"
-
-int main() {
-    auto elastic_node = ElasticNode<std::pair<int, int>>::Get(10, NodeType::LeafNode);
-    elastic_node->FreeElasticNode();
-
-    return 0;
-}

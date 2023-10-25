92944e7037923d9674400b5c026b03ef57e49878
Jacob Sherin <jacob@protoship.io>
2023-10-25 15:04:08 +0530

    construct & free an elastic node

--- src/bplustree.h
+++ src/bplustree.h
@@ -6,9 +6,9 @@ namespace bplustree {
         InnerType = 0, LeafType = 1
     };
 
-    class Node {
+    class BaseNode {
     public:
-        Node(NodeType p_type, int p_max_size) :
+        BaseNode(NodeType p_type, int p_max_size) :
                 type_{p_type},
                 max_size_{p_max_size},
                 curr_size_{0} {}
@@ -20,5 +20,45 @@ namespace bplustree {
         int max_size_;
         int curr_size_;
     };
+
+    template<typename ElementType>
+    class ElasticNode : public BaseNode {
+    public:
+        ElasticNode(NodeType p_type, int p_max_size) :
+                BaseNode(p_type, p_max_size) {}
+
+        /**
+         * Static helper to allocate storage for an elastic node.
+         */
+        static ElasticNode *Get(NodeType p_type, int p_max_size) {
+            auto *alloc = new char[sizeof(ElasticNode) + p_max_size * sizeof(ElementType)];
+
+            /**
+             * Construct elastic node in allocated storage
+             * https://en.cppreference.com/w/cpp/language/new#Placement_new
+             */
+            auto elastic_node = reinterpret_cast<ElasticNode *>(alloc);
+            new(elastic_node) ElasticNode(p_type, p_max_size);
+
+            return elastic_node;
+        }
+
+        /**
+         * Free elastic node
+         */
+        void FreeElasticNode() {
+            ElasticNode *alloc = this;
+            delete[] reinterpret_cast<char *>(alloc);
+        }
+
+    private:
+        /*
+         * Struct hack (flexible array member)
+         * https://developers.redhat.com/articles/2022/09/29/benefits-limitations-flexible-array-members
+         */
+        ElementType start_[0];
+    };
+
+
 }
 #endif //BPLUSTREE_H

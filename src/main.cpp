ca623cf845750e58874a1d02a07100e86c23aa5f
Jacob Sherin <jacob@protoship.io>
2023-10-31 19:43:31 +0530

    assert

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,25 @@
-#include <iostream>
-#include "bplustree.h"
+//#include <iostream>
+//#include "bplustree.h"
 
-using bplustree::BaseNode;
-using bplustree::NodeType;
+//using bplustree::BaseNode;
+//using bplustree::NodeType;
+//using bplustree::BPlusTree;
+
+#include "macros.h"
 
 int main() {
-    auto node = BaseNode(NodeType::InnerType, 100);
-    std::cout << static_cast<int>(node.GetType()) << std::endl;
+//    auto tree = BPlusTree(5, 5);
+//    tree.Insert(std::make_pair(150, 456));
+//    tree.Insert(std::make_pair(123, 456));
+//    tree.Insert(std::make_pair(100, 456));
+//    tree.Insert(std::make_pair(110, 456));
+//    tree.Insert(std::make_pair(130, 456));
+//    tree.Insert(std::make_pair(140, 456));
+//    tree.Insert(std::make_pair(135, 456));
+
+//    std::cout << "main ran" << std::endl;
+
+    BPLUSTREE_ASSERT(1123 == 2111, "1 equals 2");
+
     return 0;
 }

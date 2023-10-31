09b4676ff3e46a0bc21302cbc1aac55d1615a912
Jacob Sherin <jacob@protoship.io>
2023-10-31 21:43:11 +0530

    minor

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,21 @@
-//#include <iostream>
-//#include "bplustree.h"
-
-//using bplustree::BaseNode;
-//using bplustree::NodeType;
-//using bplustree::BPlusTree;
-
-#include "macros.h"
+#include <iostream>
+#include "bplustree.h"
 
 int main() {
-//    auto tree = BPlusTree(5, 5);
-//    tree.Insert(std::make_pair(150, 456));
-//    tree.Insert(std::make_pair(123, 456));
-//    tree.Insert(std::make_pair(100, 456));
-//    tree.Insert(std::make_pair(110, 456));
-//    tree.Insert(std::make_pair(130, 456));
-//    tree.Insert(std::make_pair(140, 456));
-//    tree.Insert(std::make_pair(135, 456));
-
-//    std::cout << "main ran" << std::endl;
-
-    BPLUSTREE_ASSERT(1123 == 2111, "1 equals 2");
+    auto tree = bplustree::BPlusTree(4, 4);
+    tree.Insert(std::make_pair(150, 456));
+    tree.Insert(std::make_pair(123, 456));
+    tree.Insert(std::make_pair(100, 456));
+    // try duplicate insertion
+    tree.Insert(std::make_pair(150, 456));
+    tree.Insert(std::make_pair(123, 456));
+    tree.Insert(std::make_pair(100, 456));
+    // insert one more
+    tree.Insert(std::make_pair(110, 456));
+    // from here on nothing will be inserted
+    tree.Insert(std::make_pair(130, 456));
+    tree.Insert(std::make_pair(140, 456));
+    tree.Insert(std::make_pair(135, 456));
 
     return 0;
 }

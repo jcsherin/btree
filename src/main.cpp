5ca8dfe862650c3a9f99647bafea053c59de0317
Jacob Sherin <jacob@protoship.io>
2023-11-01 19:23:26 +0530

    fanout math trial

--- src/main.cpp
+++ src/main.cpp
@@ -17,5 +17,24 @@ int main() {
     tree.Insert(std::make_pair(140, 456));
     tree.Insert(std::make_pair(135, 456));
 
+    int fanout_even = 8;
+    int fanout_even_max = fanout_even - 1;
+
+    int fanout_odd = 9;
+    int fanout_odd_max = fanout_odd - 1;
+
+    std::cout << "fanout: " << fanout_even
+              << " max: " << fanout_even_max
+              << " min: " << fanout_even_max / 2
+              << " ceil(min): " << (fanout_even_max + 2 - 1) / 2
+              << " ceil(min no-overflow): " << 1 + ((fanout_even_max - 1) / 2)
+              << std::endl;
+
+    std::cout << "fanout: " << fanout_odd
+              << " max: " << fanout_odd_max
+              << " min: " << fanout_odd_max / 2
+              << " ceil(min): " << (fanout_odd_max + 2 - 1) / 2
+              << " ceil(min no-overflow): " << 1 + ((fanout_odd_max - 1) / 2)
+              << std::endl;
     return 0;
 }

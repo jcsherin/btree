fd88c2f025b6f9c2feaca828f03e02a22214f58c
Jacob Sherin <jacob@protoship.io>
2023-12-20 22:13:05 +0530

    test: delete all keys

--- test/btree_delete_test.cpp
+++ test/btree_delete_test.cpp
@@ -32,6 +32,23 @@ namespace bplustree {
         EXPECT_FALSE(index.Delete(std::make_pair(4, 4)));
     }
 
+    TEST(BPlusTreeDeleteTest, DeleteAllKeys) {
+        BPlusTree index{3, 4};
+
+        for (int i = 0; i < 4; ++i) {
+            index.Insert(std::make_pair(i, i));
+        }
+        EXPECT_NE(index.GetRoot(), nullptr);
+
+        for (int i = 0; i < 4; ++i) {
+            auto deleted = index.Delete(std::make_pair(i, i));
+
+            EXPECT_TRUE(deleted);
+            EXPECT_EQ(index.FindValueOfKey(i), std::nullopt);
+        }
+        EXPECT_EQ(index.GetRoot(), nullptr);
+    }
+
     TEST(BPlusTreeDeleteTest, RootUnderflowAllowed) {
         BPlusTree index{3, 4};
 

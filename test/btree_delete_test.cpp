e7d43d7cf1d894d25690c9778d87c0791677bb52
Jacob Sherin <jacob@protoship.io>
2023-12-20 20:21:31 +0530

    refactor test case

--- test/btree_delete_test.cpp
+++ test/btree_delete_test.cpp
@@ -3,23 +3,27 @@
 #include "../src/bplustree.h"
 
 namespace bplustree {
-    TEST(BPlusTreeDeleteTest, SimpleDelete) {
+    TEST(BPlusTreeDeleteTest, DeleteAnExistingKey) {
         BPlusTree index{3, 4};
+
+        // Empty B+Tree
+        EXPECT_EQ(index.GetRoot(), nullptr);
 
         for (int i = 0; i < 4; ++i) {
             index.Insert(std::make_pair(i, i));
+
             EXPECT_EQ(index.FindValueOfKey(i), i);
         }
 
-        auto root = static_cast<ElasticNode<KeyValuePair> *>(index.GetRoot());
-        EXPECT_NE(root, nullptr);
+        EXPECT_NE(index.GetRoot(), nullptr);
 
         for (int i = 0; i < 4; ++i) {
             auto deleted = index.Delete(std::make_pair(i, i));
+
             EXPECT_TRUE(deleted);
+            EXPECT_EQ(index.FindValueOfKey(i), std::nullopt);
         }
 
-        auto new_root = static_cast<ElasticNode<KeyValuePair> *>(index.GetRoot());
-        EXPECT_EQ(new_root, nullptr);
+        EXPECT_EQ(index.GetRoot(), nullptr);
     }
 }

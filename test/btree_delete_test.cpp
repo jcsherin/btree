08c6c89b1faa2168524666d52f70336882aad362
Jacob Sherin <jacob@protoship.io>
2023-12-20 20:32:28 +0530

    test: root(leaf) underflow allowed

--- test/btree_delete_test.cpp
+++ test/btree_delete_test.cpp
@@ -26,4 +26,23 @@ namespace bplustree {
 
         EXPECT_EQ(index.GetRoot(), nullptr);
     }
+
+    TEST(BPlusTreeDeleteTest, RootUnderflowAllowed) {
+        BPlusTree index{3, 4};
+
+        index.Insert(std::make_pair(1, 1));
+        index.Insert(std::make_pair(2, 2));
+
+        ASSERT_EQ(index.GetRoot()->GetType(), NodeType::LeafType);
+
+        auto root = static_cast<LeafNode *>(index.GetRoot());
+        ASSERT_EQ(root->GetCurrentSize(), 2);
+        ASSERT_EQ(root->GetMinSize(), 2);
+
+        index.Delete(std::make_pair(1, 1));
+        EXPECT_EQ(index.FindValueOfKey(1), std::nullopt);
+
+        ASSERT_EQ(root->GetCurrentSize(), 1);
+        EXPECT_EQ(root, index.GetRoot());
+    }
 }

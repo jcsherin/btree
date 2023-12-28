f90641b9490095f940caaa918a4af921cfed9a83
Jacob Sherin <jacob@protoship.io>
2023-12-28 20:09:22 +0530

    test: insertions in random order

--- test/btree_insert_test.cpp
+++ test/btree_insert_test.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <numeric>
 #include "../src/bplustree.h"
+#include <random>
 
 namespace bplustree {
     TEST(BPlusTreeInsertTest, InsertAndFetchEveryKey) {
@@ -26,6 +27,36 @@ namespace bplustree {
             EXPECT_EQ((*iter).first, j--);
         }
     }
+
+    TEST(BPlusTreeInsertTest, InsertInRandomOrder) {
+        BPlusTree index{3, 4};
+
+        std::vector<int> items(10000);
+        std::iota(items.begin(), items.end(), 0);
+
+        std::random_device rd;
+        std::mt19937 g(rd());
+
+        std::shuffle(items.begin(), items.end(), g);
+
+        for (auto &i: items) {
+            index.Insert(std::make_pair(i, i));
+
+            ASSERT_EQ(index.FindValueOfKey(i), i);
+        }
+        EXPECT_NE(index.GetRoot(), nullptr);
+
+        int i = 0;
+        for (auto iter = index.Begin(); iter != index.End(); ++iter) {
+            EXPECT_EQ((*iter).first, i++);
+        }
+
+        int j = items.size() - 1;
+        for (auto iter = index.RBegin(); iter != index.REnd(); --iter) {
+            EXPECT_EQ((*iter).first, j--);
+        }
+    }
+
 
     TEST(BPlusTreeInsertTest, IsEmptyInitially) {
         BPlusTree index{4, 5};

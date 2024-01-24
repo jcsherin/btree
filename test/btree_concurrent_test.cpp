65879ace6a8537c547bf302c06dd215337c3868f
Jacob Sherin <jacob@protoship.io>
2024-01-24 22:22:29 +0530

    adds concurrent delete test

--- test/btree_concurrent_test.cpp
+++ test/btree_concurrent_test.cpp
@@ -37,4 +37,42 @@ namespace bplustree {
         }
         EXPECT_EQ(i, keys.size());
     }
+
+    TEST(BPlusTreeConcurrentTest, ConcurrentDeletes) {
+        BPlusTree index{3, 4};
+
+        std::vector<int> keys(10000);
+        std::iota(keys.begin(), keys.end(), 0);
+
+        for (auto &key: keys) {
+            index.Insert(std::make_pair(key, key));
+        }
+
+        int worker_threads = 8;
+        int keys_per_worker = keys.size() / worker_threads;
+
+        auto index_delete_workload = [&](uint32_t worker_id) {
+            int start = keys_per_worker * worker_id;
+            int end = keys_per_worker * (worker_id + 1);
+
+            for (int i = start; i < end; ++i) {
+                index.Delete(i);
+                EXPECT_EQ(index.MaybeGet(i), std::nullopt);
+            }
+        };
+
+        std::vector<std::thread> workers;
+        for (uint32_t worker_id = 0; worker_id < worker_threads; ++worker_id) {
+            workers.push_back(std::thread(index_delete_workload, worker_id));
+        }
+
+        for (uint32_t worker_id = 0; worker_id < worker_threads; ++worker_id) {
+            workers[worker_id].join();
+        }
+
+        // Empty B+Tree
+        EXPECT_EQ(index.Begin(), index.End());
+
+        // FIXME: Mutex lock failed when reinserting values into B+Tree
+    }
 }

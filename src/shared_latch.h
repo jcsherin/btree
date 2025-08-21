7f0a4d914b38f8a4cbc10ae3bccd3e49b5c8cddb
Sherin Jacob <jacob@protoship.io>
2025-08-21 21:02:13 +0530

    feat: enable latch debugging for tests
    Adds a new ENABLE_LATCH_DEBUGGING compile-time flag when enabled will
    keep track of the lock and unlock calls for both shared and exclusive
    latches. This is done to catch mistakes in code when a lock call does
    not have a matching unlock call, or the other way around. When the
    tests are run and the SharedLatch destructor is called, if the counts
    are not zero, then the tests will fail.

--- src/shared_latch.h
+++ src/shared_latch.h
@@ -1,4 +1,7 @@
 #include <shared_mutex>
+#include <cassert>
+#include <cstdio>
+#include <atomic>
 
 /**
  * A wrapper around std::shared_mutex
@@ -7,18 +10,63 @@ namespace bplustree {
 
     class SharedLatch {
     public:
-        void LockExclusive() { latch_.lock(); }
+#ifdef ENABLE_LATCH_DEBUGGING
+        ~SharedLatch() {
+            if (exclusive_lock_count_ != 0) {
+                fprintf(stderr, "exclusive_lock_count_ is %d, expected 0\n", exclusive_lock_count_.load());
+            }
+            assert(exclusive_lock_count_ == 0);
+            if (shared_lock_count_ != 0) {
+                fprintf(stderr, "shared_lock_count_ is %d, expected 0\n", shared_lock_count_.load());
+            }
+            assert(shared_lock_count_ == 0);
+        }
+#endif
 
-        void LockShared() { latch_.lock_shared(); }
 
-        void UnlockExclusive() { latch_.unlock(); }
+        void LockExclusive() {
+            latch_.lock();
+#ifdef ENABLE_LATCH_DEBUGGING
+            exclusive_lock_count_++;
+#endif
+        }
 
-        void UnlockShared() { latch_.unlock_shared(); }
+        void LockShared() {
+            latch_.lock_shared();
+#ifdef ENABLE_LATCH_DEBUGGING
+            shared_lock_count_++;
+#endif
+        }
 
-        bool TryLockShared() { return latch_.try_lock_shared(); }
+        void UnlockExclusive() {
+#ifdef ENABLE_LATCH_DEBUGGING
+            exclusive_lock_count_--;
+#endif
+            latch_.unlock();
+        }
+
+        void UnlockShared() {
+#ifdef ENABLE_LATCH_DEBUGGING
+            shared_lock_count_--;
+#endif
+            latch_.unlock_shared();
+        }
+
+        bool TryLockShared() {
+            bool success = latch_.try_lock_shared();
+#ifdef ENABLE_LATCH_DEBUGGING
+            if (success) {
+                shared_lock_count_++;
+            }
+#endif
+            return success;
+        }
 
     private:
         std::shared_mutex latch_;
+#ifdef ENABLE_LATCH_DEBUGGING
+        std::atomic<int> exclusive_lock_count_{0};
+        std::atomic<int> shared_lock_count_{0};
+#endif
     };
 }
-
